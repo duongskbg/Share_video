4ef2931eaf3cffe4f8820fd7214a667fa07949a8
Huu Duong <33078419+duongskbg@users.noreply.github.com>
2024-04-27 06:24:59 +0700

    Update posts_controller.rb
    delete admin role

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy like dislike]
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :is_admin!, except: [:index,:show, :like, :dislike]
   before_action :authenticate_user!, only: [:like, :dislike]
   # GET /posts or /posts.json
   def index
